d4b80a2c8f49a4484fb6654ffa2f0ebbbd73c5bd
oğuz yürken <oguz.yurken@gmail.com>
2022-03-08 06:13:11 +0300

    local storage update

--- Movie Seat Booking/script.js
+++ Movie Seat Booking/script.js
@@ -6,9 +6,20 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value; //+ekleyerek string degeri numbera cevirdik
 
+//Save Selected Movie Index and Price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 //Update total and count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat))
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
+
     //secilen koltuk sayisinin uzunlugunu bir degiskene atiyoruz boylece kac koltuk sectigimizi gorebiliyoruz
     const selectedSeatsCount = selectedSeats.length;
 
@@ -21,6 +32,9 @@ function updateSelectedCount() {
 //Movie Select Event
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+
+    setMovieData(e.target.selectedIndex, e.target.value);
+
     updateSelectedCount();
 })
 
