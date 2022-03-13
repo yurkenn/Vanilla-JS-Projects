ae7810dbf0d3f5f8407eb3cf7bfdd7ea5b68ba2b
oğuz yürken <oguz.yurken@gmail.com>
2022-03-13 11:07:33 +0300

    css add new animation | js eventlisteners update

---
+++ Menu Slider-and-Modal/script.js
@@ -0,0 +1,16 @@
+const toggle = document.getElementById('toggle')
+const close = document.getElementById('close')
+const open = document.getElementById('open')
+const modal = document.getElementById('modal')
+
+//toggle nav
+toggle.addEventListener('click', () => document.body.classList.toggle('show-nav'))
+
+//show modal
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+
+//Hide modal
+close.addEventListener('click', () => modal.classList.remove('show-modal'))
+
+//hide modal 
+window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal') : false)
